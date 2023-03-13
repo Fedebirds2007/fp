cb9cdeb3e91950c4e7a96e74c0004e2ac3e06958
Fedebirds2007 <fedeclave@hotmail.com>
2023-03-13 21:15:59 +0100

    a

--- mods/B.B EX/stages/night-ex.lua
+++ mods/B.B EX/stages/night-ex.lua
@@ -1,32 +1,32 @@
 function onCreate()
     
-    makeLuaSprite('back', 'BG/week1/Amor/BG1', -500, -300)
+    makeLuaSprite('back', 'Amor_BG1', -500, -300)
 	addLuaSprite('back', false)
 	setScrollFactor('back', 0.9, 0.9)
 	scaleObject('back', 0.6, 0.6);
 	
-    makeLuaSprite('center', 'BG/week1/Amor/BG2', -350, -370)
+    makeLuaSprite('center', 'Amor_BG2', -350, -370)
 	addLuaSprite('center', false)
 	setScrollFactor('center', 1, 1)
 	scaleObject('center', 0.7, 0.7);
 
-    makeLuaSprite('front', 'BG/week1/Amor/BG3', -500, -300)
+    makeLuaSprite('front', 'Amor_BG3', -500, -300)
 	addLuaSprite('front', false)
 	setScrollFactor('front', 1, 1)
 	scaleObject('front', 0.9, 0.9);
 	
-	makeLuaSprite('bgg', 'BG/week1/Amor/BG4', -550, -260)
+	makeLuaSprite('bgg', 'Amor_BG4', -550, -260)
 	addLuaSprite('bgg', false)
 	setScrollFactor('bgg', 1, 1)
 	scaleObject('bgg', 0.58, 0.58);
 	
-	makeAnimatedLuaSprite('pixelthing', 'BG/week1/amor/pixelthing', -650, -300)
+	makeAnimatedLuaSprite('pixelthing', 'pixelthing', -650, -300)
     addAnimationByPrefix('pixelthing', 'idle', 'pixelthing', 24, true)
 	setScrollFactor('pixelthing', 1, 1)
 	addLuaSprite('pixelthing', false)
 	scaleObject('pixelthing', 2, 2);
 	
-	makeAnimatedLuaSprite('pc', 'BG/week1/amor/pc', 170, 220)
+	makeAnimatedLuaSprite('pc', 'pc', 170, 220)
     addAnimationByPrefix('pc', 'idle', 'PC idle', 24, true)
     addAnimationByPrefix('pc', 'left', 'PC Note LEFT', 24, true)
     addAnimationByPrefix('pc', 'down', 'PC Note DOWN', 24, true)
@@ -36,19 +36,19 @@ function onCreate()
 	addLuaSprite('pc', false)
 	scaleObject('pc', 0.8, 0.8);
 	
-	makeLuaSprite('ARE', 'BG/week1/Amor/ARE', -250, -100)
+	makeLuaSprite('ARE', 'ARE', -250, -100)
 	addLuaSprite('ARE', true)
 	setScrollFactor('ARE', 0, 0)
 	scaleObject('ARE', 1.4, 1.4);
 	setProperty('ARE.visible', false)
 	
-	makeLuaSprite('YOU', 'BG/week1/Amor/YOU', -250, -100)
+	makeLuaSprite('YOU', 'YOU', -250, -100)
 	addLuaSprite('YOU', true)
 	setScrollFactor('YOU', 0, 0)
 	scaleObject('YOU', 1.4, 1.4);
 	setProperty('YOU.visible', false)
 	
-	makeLuaSprite('READY', 'BG/week1/Amor/READY', -250, -100)
+	makeLuaSprite('READY', 'READY', -250, -100)
 	addLuaSprite('READY', true)
 	setScrollFactor('READY', 0, 0)
 	scaleObject('READY', 1.4, 1.4);
